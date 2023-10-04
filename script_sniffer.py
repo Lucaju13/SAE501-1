0d937e6f5ca223a5c01a0b085e90aa0ef6be8313
tylasp <86302112+tylasp@users.noreply.github.com>
2023-10-04 10:32:57 +0200

    Focus sur le DHCP

--- script_sniffer.py
+++ script_sniffer.py
@@ -1,6 +1,6 @@
 import scapy.all as scapy
 
-#Liste des noms des types de paquet DHCP
+# Liste des noms des types de paquet DHCP
 type_dhcp = ["","Discover","Offer","Request","Decline","ack","nak","release","inform","fore_renew","lease_query","lease_unassigned","lease_unknown","lease_active"]
 
 def packet_handler(packet):
@@ -9,21 +9,18 @@ def packet_handler(packet):
         dst_ip = packet[scapy.IP].dst
         packet_id = packet[scapy.IP].id
         timestamp = packet.time
-        print(f"Timestamp: {timestamp}, Source IP: {src_ip} -> Destination IP: {dst_ip}, Packet ID: {packet_id}")
-        if packet.haslayer(scapy.ICMP):
-           icmp_type = packet[scapy.ICMP].type
-           if icmp_type == 0:
-               print("Réussite")
-           else:
-               print("Échec")
-        elif packet.haslayer(scapy.DHCP):
+        # Suppression du test ICMP pour ne garder que la trame DHCP
+        if packet.haslayer(scapy.DHCP):
             dhcp_message_type = packet[scapy.DHCP].options[0][1]
-            dhcp_type = type_dhcp[dhcp_message_type] #Récupère le nom du paquet dhcp selon le type
-            print(dhcp_type)
+            dhcp_type = type_dhcp[dhcp_message_type]
+            print(f"Timestamp: {timestamp}, Source IP: {src_ip} -> Destination IP: {dst_ip}, Packet ID: {packet_id}, Trame DHCP: {dhcp_type}")
+            # Affiche désormais toutes les informations lorsque la trame est un DHCP + affiche en même temps le type DHCP
+            
+			# Il faut envoyer ensuite ces informations sur la base de données.
 
 def main(interface):
     scapy.sniff(iface=interface, prn=packet_handler)
 
 if __name__ == "__main__":
-    interface = "enp0s31f6"  # Remplacez par le nom de votre interface réseau
+    interface = "enp0s31f6"  # Modifier script plus tard pour qu'il prenne l'interface active ou demander interface ?
     main(interface)
