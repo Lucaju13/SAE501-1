bd03f5c468400904cd9b69201dedd1aef3cb02be
tylasp <86302112+tylasp@users.noreply.github.com>
2023-10-06 11:35:57 +0200

    Test connectivité avec la BDD Sqlite3

--- script_sniffer.py
+++ script_sniffer.py
@@ -1,5 +1,12 @@
 import scapy.all as scapy
 from datetime import datetime
+import sqlite3
+######### Joindre à SQL #########
+con = sqlite3.connect("sae501.db") #Se connecter au fichier de la bdd sqlite3
+cursor = con.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';") # Permet d'afficher les tables pour vérifier la connectivité d'abord
+print(cursor.fetchall())
+######### Joindre à SQL #########
 
 type_dhcp = ["","Discover","Offer","Request","Decline","ack","nak","release","inform","fore_renew","lease_query","lease_unassigned","lease_unknown","lease_active"]
 
@@ -14,9 +21,8 @@ def packet_handler(packet):
         if packet.haslayer(scapy.DHCP):
             dhcp_message_type = packet[scapy.DHCP].options[0][1]
             dhcp_type = type_dhcp[dhcp_message_type]
-            date = datetime.fromtimestamp(timestamp, tz = None) # Calcule la date depuis la valeur du timestamp.
+            date = datetime.fromtimestamp(timestamp, tz = None) 
             print(f"Timestamp: {timestamp}, Date: {date}, Source IP: {src_ip} -> Destination IP: {dst_ip}, Packet ID: {packet_id}, Trame DHCP: {dhcp_type}, Source Mac: {src_mac} -> Destination Mac: {dst_mac}")
-			# J'envoie la date en plus du timestamp
             
 			# Il faut envoyer ensuite ces informations sur la base de données.
 
