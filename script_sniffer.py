b2cc69b590f23e82b389d1caca1968dd6c9b2f00
tylasp <86302112+tylasp@users.noreply.github.com>
2023-10-10 10:23:51 +0200

    Mauvaise variable réglé

--- script_sniffer.py
+++ script_sniffer.py
@@ -20,7 +20,7 @@ def packet_handler(packet):
             dhcp_message_type = packet[scapy.DHCP].options[0][1]
             dhcp_type = type_dhcp[dhcp_message_type]
             date = datetime.fromtimestamp(timestamp, tz = None) 
-            print(f"Timestamp: {timestamp}, Date: {date}, Source IP: {Src_IP} -> Destination IP: {Src_IP}, Packet ID: {Packet_ID}, Trame DHCP: {dhcp_type}, Source Mac: {Src_MAC} -> Destination Mac: {Dst_MAC}") 
+            print(f"Timestamp: {timestamp}, Date: {date}, Source IP: {Src_IP} -> Destination IP: {Dst_IP}, Packet ID: {Packet_ID}, Trame DHCP: {dhcp_type}, Source Mac: {Src_MAC} -> Destination Mac: {Dst_MAC}") 
             sqlite_insert_query = """INSERT INTO data
                           (Src_IP, Dst_IP, Src_MAC, Dst_MAC, Packet_ID, Time, Heure, Type_Trame) 
                            VALUES 
