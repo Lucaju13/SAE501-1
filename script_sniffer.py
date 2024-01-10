1a4bf87fd7c83243541cfa36cfb6eb27abf85208
tylasp <86302112+tylasp@users.noreply.github.com>
2024-01-10 14:38:28 +0100

    Script pour qu'il agisse sur le gui

--- script_sniffer.py
+++ script_sniffer.py
@@ -1,14 +1,23 @@
 import scapy.all as scapy
 from datetime import datetime
 import sqlite3
-######### Joindre à SQL #########
-con = sqlite3.connect("sae501.db")
-cursor = con.cursor()
-######### Joindre à SQL #########
+
 type_dhcp = ["","Discover","Offer","Request","Decline","ack","nak","release","inform","force_renew","lease_query","lease_unassigned","lease_unknown","lease_active"]
 
+sniffing_active = True  # Variable qui sert à renseigner si le sniffing doit être actif ou pas
+
+def stop_sniffing():
+	global sniffing_active
+	sniffing_active = False
+
 def packet_handler(packet):
-	print("test2") # Cette partie tourne en continue ensuite
+	print("test") # Print qui me permet de voir si le script tourne puis si il est arrêté
+	con = sqlite3.connect("sae501.db") #
+	cursor = con.cursor() #
+	global sniffing_active
+	if not sniffing_active:
+		return
+
 	if packet.haslayer(scapy.IP):
 		Src_IP = packet[scapy.IP].src
 		Dst_IP = packet[scapy.IP].dst
@@ -16,8 +25,8 @@ def packet_handler(packet):
 		Dst_MAC = packet[scapy.Ether].dst
 		Packet_ID = packet[scapy.IP].id
 		timestamp = packet.time
+		
 		if packet.haslayer(scapy.DHCP) and packet[scapy.DHCP].options[0][1] != 1:
-		# Réparation du filtre. Le script essayer de trouver la partie DHCP de toutes les trames.
 			dhcp_message_type = packet[scapy.DHCP].options[0][1]
 			dhcp_type = type_dhcp[dhcp_message_type]
 			date = datetime.fromtimestamp(timestamp, tz = None) 
@@ -29,14 +38,15 @@ def packet_handler(packet):
 			data_tuple = (Src_IP, Dst_IP, Src_MAC, Dst_MAC, Packet_ID, timestamp, date, dhcp_type) 
 			cursor.execute(sqlite_insert_query, data_tuple) 
 			con.commit()
+			cursor.close() #
+			con.close() #
 			
 def main(interface):
 	scapy.sniff(iface=interface, prn=packet_handler)
+	
 if __name__ == "__main__":
-	print("test1") # Cette partie n'est exécuté qu'une fois
 	interface = "eno1"  # On n'a plus besoin de modifier l'adresse si on fait sur un raspberry
 	main(interface)
     
 #Notes: Faire en sorte que le script tourne selon une fonction en renseignant une durée de script.
 #Et transformer le script gui pour tester un bouton pour lancer le script sur une 1ère durée
-
