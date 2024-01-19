d77b6c5de5a283182b7e9d453ea8bc8bbc56cdb1
tylasp <86302112+tylasp@users.noreply.github.com>
2024-01-19 16:34:30 +0100

    Suppression lignes inutiles + commentaires

--- script_sniffer.py
+++ script_sniffer.py
@@ -1,23 +1,20 @@
+## Modules ##
 import scapy.all as scapy
 from datetime import datetime
 import sqlite3
 
-type_dhcp = ["","Discover","Offer","Request","Decline","ack","nak","release","inform","force_renew","lease_query","lease_unassigned","lease_unknown","lease_active"]
+## Variables ##
+type_dhcp = ["","Discover","Offer","Request","Decline","ack","nak","release","inform","force_renew","lease_query","lease_unassigned","lease_unknown","lease_active"] # Liste des types de paquets DHCP
 sniffing_active = True  # Variable qui sert à renseigner si le sniffing doit être actif ou pas
 
-# Fonction non fonctionnelle supprimée
-
+## Fonctions ##
 def packet_handler(packet, app_instance):
-	if not app_instance.sniffing_active: # Le script s'arrête si le flag est désactivé, donc si le bouton stop est cliqué
+	if not app_instance.sniffing_active: # Le script ne s'exécute pas si le flag est désactivé, donc si le bouton stop est cliqué
 		return
-	print("test")
-	con = sqlite3.connect("sae501.db")
+	con = sqlite3.connect("sae501.db") # Connexion à la base de données
 	cursor = con.cursor()
-	global sniffing_active
-	if not sniffing_active:
-		return
 
-	if packet.haslayer(scapy.IP):
+	if packet.haslayer(scapy.IP): # On récupère les informations IP, MAC et timestamp du paquet si il y a une couche IP
 		Src_IP = packet[scapy.IP].src
 		Dst_IP = packet[scapy.IP].dst
 		Src_MAC = packet[scapy.Ether].src 
@@ -25,28 +22,32 @@ def packet_handler(packet, app_instance):
 		Packet_ID = packet[scapy.IP].id
 		timestamp = packet.time
 		
-		if packet.haslayer(scapy.DHCP) and packet[scapy.DHCP].options[0][1] != 1:
+		if packet.haslayer(scapy.DHCP) and packet[scapy.DHCP].options[0][1] != 1: # On récupère l'heure, le type de message et le type de trame DHCP si c'est une trame DHCP
 			dhcp_message_type = packet[scapy.DHCP].options[0][1]
 			dhcp_type = type_dhcp[dhcp_message_type]
 			date = datetime.fromtimestamp(timestamp, tz = None) 
+			# Envoie dans la console des informations récupérées
 			print(f"Timestamp: {timestamp}, Date: {date}, Source IP: {Src_IP} -> Destination IP: {Dst_IP}, Packet ID: {Packet_ID}, Trame DHCP: {dhcp_type}, Source Mac: {Src_MAC} -> Destination Mac: {Dst_MAC}") 
-			sqlite_insert_query = """INSERT INTO data
+			# Création d'une insert query SQLite avec les différentes variables de la table data, les valeurs étant d'abord renseignées par des "?"
+			sqlite_insert_query = """INSERT INTO data 
 						  (Src_IP, Dst_IP, Src_MAC, Dst_MAC, Packet_ID, Time, Heure, Type_Trame) 
 						   VALUES 
 						  (?,?,?,?,?,?,?,?)"""
+			# Création d'un tuple avec les différentes valeurs récupérés, dans le même ordre auquel les variables de la table ont été renseignés.
 			data_tuple = (Src_IP, Dst_IP, Src_MAC, Dst_MAC, Packet_ID, timestamp, date, dhcp_type) 
+			# L'envoie des données avec la requête SQLite et les valeurs renseignées dans le tuple est exécuté.
 			cursor.execute(sqlite_insert_query, data_tuple) 
+			# L'envoie est confirmée.
 			con.commit()
+			# La connexion à la base de données se ferme.
 			cursor.close()
 			con.close()
 			
 def main(interface, app_instance):
-    while app_instance.sniffing_active: # Nouveau flag pour que le script continue tant que le bouton n'est pas cliqué
+    while app_instance.sniffing_active: # Flag pour que le script continue tant que le bouton n'est pas cliqué
         scapy.sniff(iface=interface, prn=lambda pkt: packet_handler(pkt, app_instance))
-	
+
+## Bloc Principal ##
 if __name__ == "__main__":
-	interface = "eno1"  # On n'a plus besoin de modifier l'adresse si on fait sur un raspberry
+	interface = "eno1"  # Interfaces rasp: eth0 et wlan0
 	main(interface)
-    
-#Notes: Faire en sorte que le script tourne selon une fonction en renseignant une durée de script.
-#Et transformer le script gui pour tester un bouton pour lancer le script sur une 1ère durée
